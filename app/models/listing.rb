51780bffde98ee8f20fc07d077328952d8f1cad6
Tim Baker <timpwbaker@gmail.com>
2018-03-20 22:36:37 +0000

    Hyperlink titles
    Hyperlinks the listing titles through to the original listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,8 @@ class Listing < ApplicationRecord
   def price
     prices.chronological.last
   end
+
+  def rm_link
+    "http://www.rightmove.co.uk/property-for-sale/property-#{self.rightmove_id}.html"
+  end
 end
