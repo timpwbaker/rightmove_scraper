7054a6508fa842ff13416d849c53dca7a28e3a8f
Tim Baker <timpwbaker@gmail.com>
2018-03-20 17:44:44 +0000

    Chrome settings for heroku

--- config/initializers/capybara_initializer.rb
+++ config/initializers/capybara_initializer.rb
@@ -2,9 +2,15 @@ require 'selenium-webdriver'
 require 'nokogiri'
 require 'capybara'
 
+options = Selenium::WebDriver::Chrome::Options.new
+chrome_bin_path = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+options.binary = chrome_bin_path if chrome_bin_path # only use custom path on heroku
+options.add_argument('--headless') # this may be optional
+
 Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
+
 Capybara.javascript_driver = :headless_chrome
 Capybara.configure do |config|
   config.default_max_wait_time = 10 # seconds
