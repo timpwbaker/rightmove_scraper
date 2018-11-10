8689c250147c1d2bd8a19cfea6cc8221bb57c8dd
Tim Baker <timpwbaker@gmail.com>
2018-11-10 10:01:10 +0000

    Some more stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,13 +1,36 @@
 class StatsController < ApplicationController
 
   def index
-    @listing_counts =
-      area.listings.group_by { |m| m.created_at.beginning_of_month }.sort
+    @counts = counts
+    @known_delisted_counts = known_delisted_counts
+    @unknown_delisted_counts = unknown_delisted_counts
+    @under_offer_counts = under_offer_counts
   end
 
   private
 
   def area
-    Area.find(params[:area_id])
+    @_area ||= Area.find(params[:area_id])
+  end
+
+  def listings
+    @_listings ||= area.listings
+  end
+
+  def counts
+    listings.group_by { |m| m.created_at.beginning_of_month }.sort
+  end
+
+  def known_delisted_counts
+    listings.known_delisted
+      .group_by { |m| m.time_delisted.beginning_of_month }.sort
+  end
+
+  def unknown_delisted_counts
+    listings.unknown_delisted
+  end
+
+  def under_offer_counts
+    listings.under_offer.group_by { |m| m.under_offer.beginning_of_month }.sort
   end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,22 @@ class Listing < ApplicationRecord
   belongs_to :area
   has_many :prices, dependent: :destroy
 
+  def self.known_delisted
+    delisted.where.not(time_delisted: nil)
+  end
+
+  def self.unknown_delisted
+    delisted.where(time_delisted: nil)
+  end
+
+  def self.delisted
+    self.where(delisted: true, under_offer: nil)
+  end
+
+  def self.under_offer
+    self.where.not(under_offer: nil)
+  end
+
   def price
     @_price ||= prices.chronological.last
   end
