4504ca5747210fb0a0a1daa8fe13ef3ac42c8ee4
Tim Baker <timpwbaker@gmail.com>
2018-11-19 22:34:22 +0000

    percentage difference of asking/sale price

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -27,6 +27,32 @@ class Listing < ApplicationRecord
     @_price ||= prices.chronological.last
   end
 
+  def current_listing_sold_price
+    relevant_years = [
+      price.created_at.year,
+      (price.created_at + 3.months).year,
+      (price.created_at - 3.months).year
+    ].uniq
+
+    sold_prices.where(year: relevant_years).first
+  end
+
+  def asking_sold_percentage
+    (current_listing_sold_price.amount.to_f/price.amount) * 100
+  end
+
+  def asking_sold_percentage_difference
+    asking_sold_percentage - 100
+  end
+
+  def sold_price_string
+    if current_listing_sold_price
+      operator = asking_sold_percentage_difference > 0 ? "+" : "-"
+      "Sold for #{asking_sold_percentage.round(2)}% of asking price \
+      (#{operator} #{asking_sold_percentage_difference.abs.round(2)}%)"
+    end
+  end
+
   def rm_link
     "http://www.rightmove.co.uk/property-for-sale/property-#{self.rightmove_id}.html"
   end
